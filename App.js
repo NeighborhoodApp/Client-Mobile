a892a291d433d71f561cf737ad9e05dddbd5c2a8
nadivaramadhani <alwafi.nadiva@gmail.com>
2020-12-14 21:22:18 +0700

    FIXING WAITING PAGES

--- App.js
+++ App.js
@@ -2,88 +2,74 @@ import React from 'react';
 import { Text, Image, TouchableOpacity, StyleSheet, Button, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-<<<<<<< HEAD
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
-import { Home, GetStarted, JoinUs, Login, Waiting, Discover, Profile, Notification } from './pages';
+import { Home, GetStarted, JoinUs, Login, Waiting, Discover, Profile, Notification, Menu } from './pages';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Avatar } from 'react-native-paper';
-
-=======
-import { Home, GetStarted, JoinUs, Login, Waiting} from './pages'
 import { Provider } from 'react-redux';
 import store from './store';
->>>>>>> 7abd48d6a38c5d9a4c6f95f321a2e879b8c778b1
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} options={{
-          title: 'Discover',
-          headerRight: () => (
-            <TouchableOpacity>
-            <Avatar.Image
-            size={40}
-            source={{
-              uri:
-                'https://i.pinimg.com/474x/73/c3/e7/73c3e7cca66a885c53718d8f3688b02c.jpg',
-            }}
-          />
-          </TouchableOpacity>
-          ),
-          headerStyle: {
-            backgroundColor: '#161C2B',
-            height: 100,
-          },
-          headerTitleContainerStyle: {
-            paddingStart: 20,
-          },
-          // headerLeft: null,
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}/>
-        <Stack.Screen name="GetStarted" component={GetStarted} options={{headerShown: false}}/>
-        <Stack.Screen name="JoinUs" component={JoinUs} options={{headerShown: false}} />
-        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-        <Stack.Screen name="Waiting" component={Waiting} options={{headerShown: false}} />
-        <Stack.Screen name="Discover" component={Discover} />
-        <Stack.Screen name="Profile" component={Profile} 
-        options={{
-          headerTitle: ()=> (<View style={styles.row}><Text style={styles.title}>Profile / Create Fees</Text><MaterialCommunityIcons name="moon-full" size={10} color="#2FBBF0"/></View>),
-          headerLeft: null,
-          headerStyle: {
-            backgroundColor: '#161C2B',
-            height: 100,
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: { 
-            alignSelf: 'center',
-            color:  '#fff',
-            }
-          }} />
-        <Stack.Screen name="Notification" component={Notification} />
-      </Stack.Navigator>
-=======
-      <Provider store={store}>
+       <Provider store={store}>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="GetStarted" component={GetStarted} options={{  headerShown: false  }}  />
-          <Stack.Screen name="JoinUs" component={JoinUs} options={{  headerShown: false  }} />
-          <Stack.Screen name="Login" component={Login} options={{  headerShown: false  }} />
-          <Stack.Screen name="Waiting" component={Waiting} options={{  headerShown: false  }} />
+          <Stack.Screen name="Home" component={Home} options={{
+            title: 'Discover',
+            headerRight: () => (
+              <TouchableOpacity>
+              <Avatar.Image
+              size={40}
+              source={{
+                uri:
+                  'https://i.pinimg.com/474x/73/c3/e7/73c3e7cca66a885c53718d8f3688b02c.jpg',
+              }}
+            />
+            </TouchableOpacity>
+            ),
+            headerStyle: {
+              backgroundColor: '#161C2B',
+              height: 100,
+            },
+            headerTitleContainerStyle: {
+              paddingStart: 20,
+            },
+            // headerLeft: null,
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}/>
+          <Stack.Screen name="GetStarted" component={GetStarted} options={{headerShown: false}}/>
+          <Stack.Screen name="JoinUs" component={JoinUs} options={{headerShown: false}} />
+          <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+          <Stack.Screen name="Waiting" component={Waiting} options={{headerShown: false}} />
+          <Stack.Screen name="Discover" component={Discover} />
+          <Stack.Screen name="Profile" component={Profile} 
+          options={{
+            headerTitle: ()=> (<View style={styles.row}><Text style={styles.title}>Profile / Create Fees</Text><MaterialCommunityIcons name="moon-full" size={10} color="#2FBBF0"/></View>),
+            headerLeft: null,
+            headerStyle: {
+              backgroundColor: '#161C2B',
+              height: 100,
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: { 
+              alignSelf: 'center',
+              color:  '#fff',
+              }
+            }} />
+          <Stack.Screen name="Notification" component={Notification} />
+          <Stack.Screen name="Menu" component={Menu} />
         </Stack.Navigator>
       </Provider>
->>>>>>> 7abd48d6a38c5d9a4c6f95f321a2e879b8c778b1
     </NavigationContainer>
   );
 }
--- pages/Waiting.js
+++ pages/Waiting.js
@@ -1,111 +1,8 @@
-<<<<<<< HEAD
-import React  from 'react'
-import { View, Text, StyleSheet, Image , TouchableOpacity } from 'react-native'
-import { useFonts, Ubuntu_300Light, Ubuntu_500Medium, Ubuntu_700Bold, } from '@expo-google-fonts/ubuntu'
-import {  Montserrat_500Medium } from '@expo-google-fonts/montserrat'
-import { AntDesign } from '@expo/vector-icons'; 
-
-function Waiting({navigation}) {   
-    let [loaded] = useFonts({
-        Ubuntu_300Light, Ubuntu_500Medium, Ubuntu_700Bold, Montserrat_500Medium 
-    });
-
-    function toDiscover(){
-        navigation.navigate('Discover')
-    }
-
-    return (
-        <View style={styles.container}>
-            <Image style={styles.waiting} source={require('../assets/waiting.png')} />
-            <View style={styles.box}> 
-                <Text style={styles.firstLine}> Hi, Tetonggo! </Text> 
-                {/* <Text style={styles.secondLine}> Please wait... {"\n"} until your account {"\n"} is verified.</Text> */}
-                <Text style={styles.secondLine}> Your account is Verified {"\n"} Click "Next" to connect{"\n"} with your neighbour</Text>
-            </View>
-            <View style={styles.footer}>
-                <View style={styles.row} onPress={toDiscover}>
-                    <Text style={styles.next} onPress={toDiscover}> Next </Text> 
-                    <TouchableOpacity style={styles.btn_next}>
-                        <AntDesign name="right" size={16} color="black" onPress={toDiscover}/>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    waiting: {
-        width: 680,
-        height: 380,
-        alignSelf: 'flex-start',
-        marginLeft: 50,
-        marginTop: 90
-    },
-    box: {
-        borderTopLeftRadius:35,
-        borderTopRightRadius:35,
-        marginTop:-20,
-        backgroundColor: '#161C2B',
-        paddingVertical: '20%',
-        width: '100%', 
-        height:'45%',
-        bottom:0
-    },
-    footer: {
-        backgroundColor: '#161C2B',
-        position: 'absolute',
-        alignSelf: 'flex-end',
-        position: 'absolute',
-        height:'5%',
-        width: '100%',
-        bottom: 0
-    },
-    firstLine: {
-        fontFamily: 'Ubuntu_700Bold',
-        fontSize: 22,
-        color: 'white',
-        textAlign: 'center',
-    
-    },
-    secondLine: {
-        marginTop: 30,
-        fontFamily: 'Montserrat_500Medium',
-        fontSize: 18,
-        color: 'white',
-        textAlign: 'center',
-        paddingBottom: 2
-    
-    },
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        bottom: 10
-    },
-    next: {
-        fontFamily: 'Montserrat_500Medium',
-        fontSize: 15,
-        color: 'white',
-        right: 20,
-    },
-    btn_next: {
-        backgroundColor: "#9CEFFE",
-        borderRadius: 50,
-        paddingVertical: 8,
-        paddingHorizontal: 8,
-        right: 10,
-        bottom: 5
-    }
-=======
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { useFonts, Ubuntu_300Light, Ubuntu_500Medium, Ubuntu_700Bold } from '@expo-google-fonts/ubuntu';
 import { Montserrat_500Medium } from '@expo-google-fonts/montserrat';
+import { AntDesign } from '@expo/vector-icons'; 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const defaultValue = {
@@ -140,13 +37,19 @@ function Waiting() {
       <View style={styles.box}>
         <Text style={styles.firstLine}> Hi, {user.fullname}! </Text>
         <Text style={styles.secondLine}>
+          {/* <Text style={styles.secondLine}> Please wait... {"\n"} until your account {"\n"} is verified.</Text> */}
           {' '}
           Please wait... {'\n'} until your account {'\n'} is verified.
         </Text>
       </View>
       <View style={styles.footer}>
-        <Text style={styles.firstLine}> Next! </Text>
-      </View>
+        <View style={styles.row} onPress={toDiscover}>
+          <Text style={styles.next} onPress={toDiscover}> Next </Text> 
+              <TouchableOpacity style={styles.btn_next}>
+                <AntDesign name="right" size={16} color="black" onPress={toDiscover}/>
+              </TouchableOpacity>
+            </View>
+        </View>
     </View>
   );
 }
@@ -158,46 +61,65 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   waiting: {
-    width: 700,
-    height: 400,
+    width: 680,
+    height: 380,
     alignSelf: 'flex-start',
     marginLeft: 50,
-    marginTop: 50,
+    marginTop: 90
   },
   box: {
-    borderTopLeftRadius: 35,
-    borderTopRightRadius: 35,
-    marginTop: -20,
+    borderTopLeftRadius:35,
+    borderTopRightRadius:35,
+    marginTop:-20,
     backgroundColor: '#161C2B',
     paddingVertical: '20%',
-    width: '100%',
-    height: '35%',
+    width: '100%', 
+    height:'45%',
+    bottom:0
   },
   footer: {
-    // marginTop:-50,
     backgroundColor: '#161C2B',
     position: 'absolute',
     alignSelf: 'flex-end',
     position: 'absolute',
-    height: '5%',
+    height:'5%',
     width: '100%',
-    bottom: 0,
+    bottom: 0
   },
   firstLine: {
     fontFamily: 'Ubuntu_700Bold',
-    paddingTop: 6,
     fontSize: 22,
     color: 'white',
     textAlign: 'center',
   },
   secondLine: {
     marginTop: 30,
-    fontFamily: 'Ubuntu_500Medium',
-    fontSize: 16,
+    fontFamily: 'Montserrat_500Medium',
+    fontSize: 18,
     color: 'white',
     textAlign: 'center',
+    paddingBottom: 2
+  
   },
->>>>>>> 7abd48d6a38c5d9a4c6f95f321a2e879b8c778b1
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    bottom: 10
+  },
+  next: {
+    fontFamily: 'Montserrat_500Medium',
+    fontSize: 15,
+    color: 'white',
+    right: 20,
+  },
+  btn_next: {
+    backgroundColor: "#9CEFFE",
+    borderRadius: 50,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    right: 10,
+    bottom: 5
+  }
 });
 
 export default Waiting;
